eb19e5f4ca9a502d645ccab45a8dcfbcc733f537
cosmosapjw-quantum <cosmosapjw@gmail.com>
2025-04-17 12:01:59 +0900

    draw  bug (incomplete, working)

--- src/python_nn_proxy.cpp
+++ src/python_nn_proxy.cpp
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include "python_nn_proxy.h"
 
 // Define the static variables that were declared in the header
@@ -8,6 +9,23 @@ std::atomic<bool> shutdown_in_progress{false};
 =======
 =======
 >>>>>>> 576f610 (darw bug (incomplete, working))
+=======
+// python_nn_proxy.cpp
+=======
+<<<<<<< HEAD
+>>>>>>> 608c6a2 (darw bug (incomplete, working))
+#include "python_nn_proxy.h"
+
+// Define the global variables
+std::recursive_mutex batch_inference_mutex;
+std::atomic<bool> shutdown_in_progress{false};
+<<<<<<< HEAD
+
+// No other implementation needed - 
+// all methods are defined in the header
+=======
+=======
+>>>>>>> 6b7c6a5 (draw  bug (incomplete, working))
 // python_nn_proxy.cpp
 #include "python_nn_proxy.h"
 
@@ -18,6 +36,11 @@ std::atomic<bool> shutdown_in_progress{false};
 // No other implementation needed - 
 // all methods are defined in the header
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 576f610 (darw bug (incomplete, working))
 =======
 >>>>>>> 576f610 (darw bug (incomplete, working))
+=======
+>>>>>>> 576f610 (darw bug (incomplete, working))
+>>>>>>> 608c6a2 (darw bug (incomplete, working))
+>>>>>>> 6b7c6a5 (draw  bug (incomplete, working))
