054c837b74dd200e90c9c6b5edeacd3e65a3a42c
cosmosapjw-quantum <cosmosapjw@gmail.com>
2025-04-17 12:15:24 +0900

    draw bug (incomplete, working)

--- src/python_nn_proxy.cpp
+++ src/python_nn_proxy.cpp
@@ -1,10 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include "python_nn_proxy.h"
 
 // Define the static variables that were declared in the header
 std::recursive_mutex batch_inference_mutex;
 std::atomic<bool> shutdown_in_progress{false};
 =======
+=======
+>>>>>>> 576f610 (darw bug (incomplete, working))
 // python_nn_proxy.cpp
 #include "python_nn_proxy.h"
 
@@ -14,4 +17,7 @@ std::atomic<bool> shutdown_in_progress{false};
 
 // No other implementation needed - 
 // all methods are defined in the header
+<<<<<<< HEAD
+>>>>>>> 576f610 (darw bug (incomplete, working))
+=======
 >>>>>>> 576f610 (darw bug (incomplete, working))
